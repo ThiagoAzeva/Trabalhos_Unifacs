572a3becaed38a08e19f49c1abc36c1df194bf76
Thiago Silvany <126682447+ThiagoAzeva@users.noreply.github.com>
2023-03-16 12:16:59 -0300

    Add files via upload

---
+++ Meu_Primeiro_Programa.java
@@ -0,0 +1,10 @@
+package JorgeC;
+
+public class Meu_Primeiro_Programa {
+	 public static void main(String[]args){
+	        
+	        System.out.println("Olá Mundo");
+	        
+	        
+	    }
+}
